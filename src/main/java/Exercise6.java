994adb8b424eba077995ea7fffa87cb77e51ce61
brooke <brookefuller96@knights.ucf.edu>
2021-06-06 21:04:15 -0400

    Initial commit

---
+++ src/main/java/Exercise6.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.time.*;
+
+
+
+import static java.lang.System.*;
+
+public class Exercise6
+{
+    public static void main(String[] args)
+    {
+        out.println("What is your current age? ");
+        Scanner input = new Scanner(in);
+        String age = input.nextLine();
+        out.println("At what age would you like to retire? ");
+        Scanner input2 = new Scanner(in);
+        String retirementAge = input2.nextLine();
+
+        try
+        {
+            int x = Integer.parseInt(age);
+            int y = Integer.parseInt(retirementAge);
+            int yearsTill = y - x;
+            int currentYear = Year.now().getValue();
+            int retireYear = yearsTill + currentYear;
+
+            if (yearsTill > 0 && x > 0)
+            {
+                System.out.println("You have " + yearsTill + " years left until you can retire. It's " + Year.now() + ", so you can retire in " + retireYear + ".");
+            }
+            if (yearsTill <= 0 && x > 0)
+            {
+                System.out.println("Congratulations! You are already retired!");
+            }
+            if(x < 0)
+            {
+                System.out.println("Oh no, it seems you have not been born yet. Try again later.");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Please enter valid numbers. Thank you.");
+        }
+
+
+    }
+}
